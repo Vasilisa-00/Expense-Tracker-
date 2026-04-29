173604447daa7f451265cac0f09fc66da40ec4dc
Vasilisa Zaytseva <vasilisazaytseva6454@gmail.com>
2026-04-29 20:21:22 +0300

    Добавила основные функции

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import json
+from datetime import datetime
+import os
+
+data_file = "expenses.json"
+
+def load_data():
+    pass
+
+def save_data(data):
+    pass
+
+
+def add_expenses():
+    pass
+
+
+def update_expenses():
+    pass
+
+
+
+
